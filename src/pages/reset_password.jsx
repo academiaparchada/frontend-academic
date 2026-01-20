a75b1b0869b734bfac45d819a824f331e1df59e1
Marco Fidel Castro Velasco <marco.castro@correounivalle.edu.co>
2026-01-19 21:41:21 -0500

    error de fetchs

--- src/pages/reset_password.jsx
+++ src/pages/reset_password.jsx
@@ -49,7 +49,7 @@ export const ResetPassword = () => {
 
     set_loading(true);
     try {
-      const response = await fetch('http://localhost:5000/api/auth/reset-password', {
+      const response = await fetch('https://api.parcheacademico.com/api/auth/reset-password', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
